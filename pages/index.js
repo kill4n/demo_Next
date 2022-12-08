a40a1bd2ea162f151bb1eec7a82f2904f1f6b5ae
Crhistian <crhistian.segura@gmail.com>
2022-12-07 19:13:19 -0500

    change intro

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStiles.headingMd}>
-        <p>Soy un gatish que ama mucho a su gatita hermosa ❤️ ❤️ </p>
+        <p>Test 001</p>
         <p>
         (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
